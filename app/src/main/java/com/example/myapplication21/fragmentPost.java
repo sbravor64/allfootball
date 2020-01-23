8cae1abdba641c9ddadfc70c138c06f2aaacb739
sbravor64 <Andresbravo97>
2020-01-23 20:46:28 +0100

    mejoras

---
+++ app/src/main/java/com/example/myapplication21/fragmentPost.java
@@ -0,0 +1,31 @@
+package com.example.myapplication21;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class fragmentPost extends Fragment {
+
+
+    public fragmentPost() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post, container, false);
+    }
+
+}
--- app/src/main/java/com/example/myapplication21/view/SiguiendoFragment.java
+++ app/src/main/java/com/example/myapplication21/view/SiguiendoFragment.java
@@ -1,6 +1,4 @@
 package com.example.myapplication21.view;
-import android.app.Application;
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,14 +9,8 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.example.myapplication21.R;
@@ -34,7 +26,6 @@ import java.util.List;
 public class SiguiendoFragment extends Fragment {
 
     AllFootballViewModel allFootballViewModel;
-    ListView listView;
 
     public SiguiendoFragment() {
         // Required empty public constructor
@@ -54,88 +45,56 @@ public class SiguiendoFragment extends Fragment {
 
         allFootballViewModel = ViewModelProviders.of(requireActivity()).get(AllFootballViewModel.class);
 
-//        EquiposAdapter equiposAdapter = new EquiposAdapter(this, );
 
-        // COMENTADO POR AHORA
+        RecyclerView recyclerViewEquipos = view.findViewById(R.id.equiposList);
 
-//        RecyclerView recyclerViewEquipos = view.findViewById(R.id.equiposList);
-//
-//        final SiguiendoFragment.EquiposAdapter equiposAdapter = new SiguiendoFragment.EquiposAdapter();
-//        recyclerViewEquipos.setAdapter(equiposAdapter);
-//
-//        allFootballViewModel.cargarEquipos().observe(getViewLifecycleOwner(), new Observer<List<Equipo>>() {
-//            @Override
-//            public void onChanged(List<Equipo> equipos) {
-//                equiposAdapter.establecerListaNoticias(equipos);
-//            }
-//        });
+        final SiguiendoFragment.EquiposAdapter equiposAdapter = new SiguiendoFragment.EquiposAdapter();
+        recyclerViewEquipos.setAdapter(equiposAdapter);
+
+        allFootballViewModel.cargarEquipos().observe(getViewLifecycleOwner(), new Observer<List<Equipo>>() {
+            @Override
+            public void onChanged(List<Equipo> equipos) {
+                equiposAdapter.establecerListaNoticias(equipos);
+            }
+        });
+
 
     }
 
-// COMENTADO POR AHORA
+    public static class EquiposAdapter extends RecyclerView.Adapter<EquiposAdapter.EquiposViewHolder> {
+        List<Equipo> equiposList;
 
-//    public class EquiposAdapter extends RecyclerView.Adapter<EquiposAdapter.EquiposViewHolder> {
-//        List<Equipo> equiposList;
-//
-//        @NonNull
-//        @Override
-//        public EquiposViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//            return new SiguiendoFragment.EquiposAdapter.EquiposViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.equipos_viewholder, parent, false));
-//        }
-//
-//        @Override
-//        public void onBindViewHolder(@NonNull SiguiendoFragment.EquiposAdapter.EquiposViewHolder holder, int position) {
-//
-//            Equipo equipos = equiposList.get(position);
-//            holder.nom.setText(equipos.getNom());
-//        }
-//
-//        @Override
-//        public int getItemCount() {
-//            return equiposList == null ? 0 : equiposList.size();
-//        }
-//
-//        void establecerListaNoticias(List<Equipo> n){
-//            equiposList = n;
-//            notifyDataSetChanged();
-//        }
-//
-//        public class EquiposViewHolder extends RecyclerView.ViewHolder {
-//
-//            TextView nom;
-//
-//            public EquiposViewHolder(@NonNull View itemView) {
-//                super(itemView);
-//                nom = itemView.findViewById(R.id.textViewNombreEquipo);
-//            }
-//        }
-//    }
+        @NonNull
+        @Override
+        public EquiposViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new SiguiendoFragment.EquiposAdapter.EquiposViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.equipos_viewholder, parent, false));
+        }
 
-}
+        @Override
+        public void onBindViewHolder(@NonNull SiguiendoFragment.EquiposAdapter.EquiposViewHolder holder, int position) {
 
+            Equipo equipos = equiposList.get(position);
+            holder.nom.setText(equipos.getNom());
+        }
 
-class  EquiposAdapter extends ArrayAdapter<String>{
+        @Override
+        public int getItemCount() {
+            return equiposList == null ? 0 : equiposList.size();
+        }
 
-    Context context;
-    String nombre[];
+        void establecerListaNoticias(List<Equipo> n){
+            equiposList = n;
+            notifyDataSetChanged();
+        }
 
-    EquiposAdapter (Context c, String nombre[]){
-        super(c, R.layout.equipos_viewholder, nombre);
-        this.context = c;
-        this.nombre = nombre;
-    }
+        public class EquiposViewHolder extends RecyclerView.ViewHolder {
 
-    @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        LayoutInflater layoutInflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = layoutInflater.inflate(R.layout.equipos_viewholder, parent, false);
+            TextView nom;
 
-        TextView textViewNombre = view.findViewById(R.id.textViewNombreEquipo);
-        textViewNombre.setText(nombre[position]);
-
-        return view;
+            public EquiposViewHolder(@NonNull View itemView) {
+                super(itemView);
+                nom = itemView.findViewById(R.id.textViewNombreEquipo);
+            }
+        }
     }
 }
-
-
